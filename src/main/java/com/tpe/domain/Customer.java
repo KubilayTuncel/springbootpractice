32f88ff44060164c17f1ec05a1386454678f2166
KubilayTuncel <kubilay.256@gmail.com>
2023-01-17 22:11:59 +0100

    1.version

---
+++ src/main/java/com/tpe/domain/Customer.java
@@ -0,0 +1,43 @@
+package com.tpe.domain;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name="t_customer")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Customer {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull(message = "First name can not be null")
+    @NotBlank(message = "First name can not be space")
+    @Size(min = 2, max = 50)
+    private String name;
+
+    @NotNull(message = "Last name can not be null")
+    @NotBlank(message = "Last name can not be space")
+    @Size(min = 2, max = 50)
+    private String lastName;
+
+    @Email//...@...
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    private String phone;
+}
